9ff6698e77b1565bbb9d214aa3f68d3f02c6d091
samarthladani133 <samarthladani133@gmail.com>
2022-08-26 17:01:11 +0530

    First Day commit

---
+++ src/main/java/com/AngularSpringbootEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootEmployeeManagerApplication.class, args);
+	}
+
+}
